fa8099808c4c9d8df9711c9290e026014a999aea
Cobb-Yoo <yhj010597@naver.com>
2021-10-18 22:31:08 +0900

    enterTitle component 추가

---
+++ frontend/src/components/enterTitle.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container>
+    <v-layout class="contentsLayout">
+      <v-row justify="center">
+        <v-col cols="8" align="center">
+          <v-card class="primary" dark>
+            <v-card-title class="green">
+              Enter Crowing Title
+            </v-card-title>
+            <v-text-field class="ma-4" label="Title" outlined></v-text-field>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "enterTitle",
+  data() {
+    return {
+      crowingTitle: "crowingTitle`s test txt",
+    };
+  },
+};
+</script>
+
+<style>
+.contentsLayout {
+  position: relative;
+  top: 250px;
+}
+</style>
---
+++ frontend/src/components/enterURL.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <div>
+          <v-text-field
+            label="Main input"
+            :rules="rules"
+            hide-details="auto"
+          ></v-text-field>
+          <v-text-field label="Another input"></v-text-field>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "enterURL",
+};
+</script>
